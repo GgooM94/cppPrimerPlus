2846431663094b429b99548dac84972e3b80ddba
GgooM <cbg1541@gmail.com>
2018-01-18 16:35:52 +0900

    chapter02_03

---
+++ chapter02/03.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void oneFunc(){
+	cout << "Three blund mice"<<endl;
+}
+void twoFunc(){
+	cout << "See how they run"<<endl;
+}
+void main(){
+	oneFunc();
+	oneFunc();
+	twoFunc();
+	twoFunc();
+}
