73518de89fe611bd6b5b4570b90e33cb67a33634
Ggoom94 <cbg1541@gmail.com>
2018-01-28 23:07:12 +0900

    chapter04_08

---
+++ chapter04/08.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+using namespace std;
+struct pizzaInf {
+	string brand;
+	double width;
+	double kg;
+};
+
+int main(void)
+{
+	pizzaInf *brand01 = new pizzaInf;
+
+	cout << "피자 지름 : ";
+	cin >> brand01->width;
+	cin.get();
+	cout << "피자 회사 : ";
+	getline(cin, brand01->brand);
+
+	cout << "피자의 중량 : ";
+	cin >> brand01->kg;
+
+	cout << "피자 회사 : " << brand01->brand << endl;
+	cout << "피자 지름 : " << brand01->width << endl;
+	cout << "피자의 중량 : " << brand01->kg << endl;
+	delete brand01;
+}
