8b70956c9b0711c6942b5965d400122ad73d4d93
Ggoom94 <cbg1541@gmail.com>
2018-01-29 04:16:14 +0900

    chapter05_08

--- chapter05/08.cpp
+++ chapter05/08.cpp
@@ -5,9 +5,8 @@ int main(void) {
 	char word[100];
 	int cnt(0);
 
-	while (strcmp(word, "done") != 0) {
-		cin >> word;
+	while (cin >> word, strcmp(word, "done") != 0) {
 		cnt++;
 	}
-	cout << "총 " << cnt - 1 << "단어가 입력되었습니다.";
+	cout << "총 " << cnt << "단어가 입력되었습니다.";
 }
